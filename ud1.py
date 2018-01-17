804c833f15f062e4c6920a191d2f89b6a75a3e97
Gireendra Sharma <gir777sam@gmail.com>
2018-01-17 16:27:44 +0530

    find the index of the element in a list
    using 'index' operation on list and using 'in'

--- ud1.py
+++ ud1.py
@@ -1,11 +1,8 @@
 #! /usr/bin/python
 def find_element(l,x):
-    u=0
-    while u<len(l):
-        if l[u]==x:
-            return u
-        u=u+1
-    return -1
-        
-        
+    i=0
+    if x in l:
+        s=l.index(x)
+        return s
+    return -1    
 print find_element([1,2,3],3)
