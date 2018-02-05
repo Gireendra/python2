5ef446717cc4db06d6876a3e4db0d9bdc065c3a2
Gireendra Sharma <gir777sam@gmail.com>
2018-02-05 22:05:26 +0530

    hash1.py
    This time i have added one more function which will update the hash table if more keywords are identified.

--- hash1.py
+++ hash1.py
@@ -1,9 +1,17 @@
 #! /usr/bin/python
+def hashtable_add(htable,key,value):
+    htable[hash_string(key,len(htable))].append([key,value])
+        return htable
+
+def hashtable_get_bucket(htable,keyword):   #only to test, to get the whole bucket to see what's in it
+    return htable[hash_string(keyword,len(htable))]
+
 def make_hashtable(nbuckets):
     table = []
     for unused in range(0,nbuckets):
         table.append([])
     return table
+
 def hash_string(keyword,buckets):
     out = 0
     for s in keyword:
