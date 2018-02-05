a498306bac63ed5e2ed7229de229c9acb94ad1b9
Gireendra Sharma <gir777sam@gmail.com>
2018-02-05 21:57:22 +0530

    hash1.py
    This time i have added hash_string function. This function will insert every keyword to it's respective bucket.

--- hash1.py
+++ hash1.py
@@ -4,3 +4,8 @@ def make_hashtable(nbuckets):
     for unused in range(0,nbuckets):
         table.append([])
     return table
+def hash_string(keyword,buckets):
+    out = 0
+    for s in keyword:
+        out = (out + ord(s)) % buckets
+    return out
